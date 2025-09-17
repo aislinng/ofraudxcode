5c313df9827e2b72c7516111529922e7a093c9a2
aislinng <a01666225@tec.mx>
2025-09-17 16:55:22 -0600

    Initial Commit

---
+++ ofraud1/ofraud1App.swift
@@ -0,0 +1,17 @@
+//
+//  ofraud1App.swift
+//  ofraud1
+//
+//  Created by Aislinn Gil on 17/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ofraud1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
